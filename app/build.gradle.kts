5d34ad793a1bf4319c2abf6c8fefe028f0a47bc8
Prithvi P Vasistha <86707196+prithvi-vasistha@users.noreply.github.com>
2026-07-10 14:06:19 +0530

    Updated the app version to the first stable build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.zig.notificationfilter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
